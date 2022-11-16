1435c0ffa4c5f7792180aade715660130c24a4d2
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-11-16 22:57:11 +0300

    Initial commit

---
+++ src/test/java/org/skypro/homework_spring/HomeworkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.homework_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
